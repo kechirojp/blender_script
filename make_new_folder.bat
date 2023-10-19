012a71e6695ee19a80456828bad253c6000d0c62
kechirojp <kechirojp@gmail.com>
2023-10-19 09:54:56 +0900

    Update make_new_folder.bat
    Change code: add conditional branch

--- make_new_folder.bat
+++ make_new_folder.bat
@@ -6,14 +6,22 @@ if not exist "%USERPROFILE%\Desktop\Blender_files" (
 )
 
 rem 「Blender_files」フォルダを親フォルダとして、その中に「meshes」「images」「studio」「studio_and_meshes」のフォルダを作成する
-rem 「meshes」フォルダ
-mkdir "%USERPROFILE%\Desktop\Blender_files\meshes"
-rem 「images」フォルダ
-mkdir "%USERPROFILE%\Desktop\Blender_files\images"
-rem 「studio」フォルダ
-mkdir "%USERPROFILE%\Desktop\Blender_files\studio"
-rem 「studio_and_meshes」フォルダ
-mkdir "%USERPROFILE%\Desktop\Blender_files\studio_and_meshes"
+rem meshesフォルダが存在しない場合は作成する
+if not exist "%USERPROFILE%\Desktop\Blender_files\meshes" (
+  mkdir "%USERPROFILE%\Desktop\Blender_files\meshes"
+)
+rem imagesフォルダが存在しない場合は作成する
+if not exist "%USERPROFILE%\Desktop\Blender_files\images" (
+  mkdir "%USERPROFILE%\Desktop\Blender_files\images"
+)
+rem studioフォルダが存在しない場合は作成する
+if not exist "%USERPROFILE%\Desktop\Blender_files\studio" (
+  mkdir "%USERPROFILE%\Desktop\Blender_files\studio"
+)
+rem studio_and_meshesフォルダが存在しない場合は作成する
+if not exist "%USERPROFILE%\Desktop\Blender_files\studio_and_meshes" (
+  mkdir "%USERPROFILE%\Desktop\Blender_files\studio_and_meshes"
+)
 
 rem 処理が完了したことを表示する
 echo 処理が完了しました。
